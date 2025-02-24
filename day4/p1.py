b645ab5a98d152623bb3d4cb26d205f1f0456bfd
prajwalcalisa <prajwalcalisa@gmail.com>
2025-02-24 14:18:38 +0530

    programs

--- day4/p1.py
+++ day4/p1.py
@@ -1,10 +1,8 @@
 sum=0
 n=input("enter the number")
 l1=len(n)
-for i in range(0,l165,2):
+for i in range(0,l1,2):
     temp=int(n[i])
-    if temp %2 ==0:
-        sum=sum+temp
-    
-print("sum=",sum)
+    sum=sum+temp
+print(sum)
 
